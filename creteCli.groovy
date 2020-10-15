5298c2bf78615d1166f426ff020cff502170ce0d
SrinivasaBharath <61141011+SrinivasaBharath@users.noreply.github.com>
2020-10-15 11:31:13 +0530

    Update creteCli.groovy

--- creteCli.groovy
+++ creteCli.groovy
@@ -1,10 +1,3 @@
-class Example {
-   static def DisplayName() {
-      println("This is how methods work in groovy");
-      println("This is an example of a simple method");
-   } 
-	
-   static void main(String[] args) {
-      DisplayName();
-   } 
+def displayName(){
+	println("Test ***");
 }
