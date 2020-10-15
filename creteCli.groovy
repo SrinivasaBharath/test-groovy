47eab24a021ddc947f0b6f18dcdc09813ad6f58d
SrinivasaBharath <61141011+SrinivasaBharath@users.noreply.github.com>
2020-10-15 11:41:46 +0530

    Update creteCli.groovy

--- creteCli.groovy
+++ creteCli.groovy
@@ -1,3 +1,10 @@
-def displayName(){
-	println("Test ***");
+pipeline {
+    agent { docker { image 'maven:3.3.3' } }
+    stages {
+        stage('build') {
+            steps {
+                sh 'mvn --version'
+            }
+        }
+    }
 }
