0cf97857f015497ea3238cfa7e83a1ae8d2f4d9a
SrinivasaBharath <61141011+SrinivasaBharath@users.noreply.github.com>
2020-10-16 09:55:32 +0530

    Update createCli.groovy

--- vars/createCli.groovy
+++ vars/createCli.groovy
@@ -1,5 +1,9 @@
 def call(Map params){
     node(){
+        
+    stage('checkout'){  
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/red-hat-storage/cephci.git']]])
+    }
     stage('build'){
         echo params.param1
         echo params.param2
